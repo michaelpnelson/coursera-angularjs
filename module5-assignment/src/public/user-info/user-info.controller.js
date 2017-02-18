367f783d7992fb3e7700b44a2f313a1820765196
Michael Nelson <michael.nelson@tasktop.com>
2017-02-17 23:31:54 -0800

    added favourite dish name and description

--- module5-assignment/src/public/user-info/user-info.controller.js
+++ module5-assignment/src/public/user-info/user-info.controller.js
@@ -10,16 +10,20 @@ function UserInfoController($http, ApiPath, UserService) {
   reg.apiPath = ApiPath;
   reg.favDishIsValid = false;
   reg.userInfo = UserService.retrieveInfo();
+  // reg.favDishName = 'q';
+  // reg.favDishDesc = 'r';
 
   reg.submit = function() {
-    reg.validateFavDish(reg.userInfo.favDish);
+    reg.validateFavDish(reg.userInfo.favDishShortName);
     UserService.storeInfo(reg.userInfo);
   }
 
-  reg.validateFavDish = function(favDish) {
-    $http.get(ApiPath + '/menu_items/' + favDish + ".json")
+  reg.validateFavDish = function(favDishShortName) {
+    $http.get(ApiPath + '/menu_items/' + favDishShortName + ".json")
     .then(function(response){
       reg.favDishNotValid = false;
+      reg.userInfo.favDishName = response.data.name;
+      reg.userInfo.favDishDesc = response.data.description;
       reg.completed = true;
     })
     .catch(function(error){
